5094599cdc9733764ec27e1c09d74c06c37a4cf3
Kate-Chua <79759626+Kate-Chua@users.noreply.github.com>
2023-06-22 18:03:49 -0700

    Cropped Hermie, made Tkinter actually do something

--- main.py
+++ main.py
@@ -6,29 +6,41 @@ window.title("Unslauy")
 window.geometry("600x600")
 
 class Instant_Frame_Probably(tk.Frame):
-    def __init__(self, master):
+    def __init__(self, master, photo):
         tk.Frame.__init__(self, master)
 
-        self.image = Image.open("Hermie_Crawling.png").convert("RGBA")
-        self.revived_hermie = self.image.copy()
+        self.image = Image.open(photo).convert("RGBA")
 
-        self.background_image = ImageTk.PhotoImage(self.image)
+        width, height = self.image.size
+        left = 0
+        upper = 0
+        lower = height
+        right = width/2
+
+        cropped_image = self.image.crop([left, upper, right, lower])
+        self.revived_hermie = cropped_image.copy()
+        self.background_image = ImageTk.PhotoImage(cropped_image)
 
         self.background = tk.Label(self, image = self.background_image)
         self.background.pack(fill = tk.BOTH, expand=tk.YES)
         self.background.bind('<Configure>', self.resize_hermie)
 
+    def Instant_Crop_Lol(self):
+        pass
+
     def resize_hermie(self, event):
         new_width_hermster = event.width
         new_height_hermster = event.height
         print(event.width, event.height)
-        self.image = self.revived_hermie.resize(new_width_hermster, new_height_hermster)
+        rescaled_width = int(new_width_hermster*0.05)
+        rescaled_height = int(new_height_hermster*0.10)
+        self.image = self.revived_hermie.resize((rescaled_width, rescaled_height))
 
         self.background_image = ImageTk.PhotoImage(self.image)
         self.background.configure(image = self.background_image)
 
 
-e = Instant_Frame_Probably(window)
+e = Instant_Frame_Probably(window, "Hermie_Crawling.png")
 e.pack(fill = tk.BOTH, expand = tk.YES)
 
 window.mainloop()
