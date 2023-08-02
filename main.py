fc7d5943ac2c6eb773aba9d3cb327bc295627048
Kate-Chua <79759626+Kate-Chua@users.noreply.github.com>
2023-08-01 18:02:17 -0700

    Update main.py
    binded hermie to the up key so it can jump
    long live the hermit crabs
    i hate tkinter

--- main.py
+++ main.py
@@ -11,23 +11,30 @@ class Instant_Frame_Probably(tk.Frame):
         self.image = Image.open(photo).convert("RGBA")
 
         width, height = self.image.size
-        left = 0
-        upper = 0
+        self.left = 0
+        self.upper = 0
         lower = height
         right = width/2
 
-        cropped_image = self.image.crop([left, upper, right, lower])
+        cropped_image = self.image.crop([self.left, self.upper, right, lower])
         self.revived_hermie = cropped_image.copy()
         self.background_image = ImageTk.PhotoImage(cropped_image)
 
         self.background = tk.Label(self, image = self.background_image)
         self.background.pack(fill = tk.BOTH, expand=tk.YES)
         #self.background.bind('<Configure>', self.resize_hermie)
+        #window.bind("<Up>", self.jump)
         overall_hermster_size = (int(32 * 3), int(32 * 3))
         self.image = self.revived_hermie.resize((overall_hermster_size))
 
         self.background_image = ImageTk.PhotoImage(self.image)
         self.background.configure(image=self.background_image)
+
+        self.x = 0
+        self.y = 0
+        self.hermie_canvas = tk.Canvas(window, width=32, height=32)
+        self.hermie_canvas.pack()
+        self.moving_hermie = self.hermie_canvas.create_image(self.x, self.y, anchor=tk.NW, image = self.background_image)
 
     def Instant_Crop_Lol(self):
         pass
@@ -43,8 +50,17 @@ class Instant_Frame_Probably(tk.Frame):
         self.background_image = ImageTk.PhotoImage(self.image)
         self.background.configure(image = self.background_image)
 
+def jump(event):
+    global e
+    #self.y = 20
+   # self.hermie_canvas.move(self.moving_hermie, self.x, self.y)
+   # self.
+  #  print(self.hermie_canvas.move)
+    e.place(y = 80)
+
 
 e = Instant_Frame_Probably(window, "Hermie_Crawling.png")
 e.pack(fill = tk.BOTH, expand = tk.YES)
+window.bind("<Up>", jump)
 
 window.mainloop()
